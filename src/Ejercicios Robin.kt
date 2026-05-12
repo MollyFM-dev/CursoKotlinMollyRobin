8d11bf03fcf21a99a078dc6df42f8b14f62c5391
SensualShinobi <robin.sanchez.sp@gmail.com>
2026-05-12 06:16:20 +0200

    Ejercicios y parámetros de retorno terminados

--- src/Ejercicios Robin.kt
+++ src/Ejercicios Robin.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 fun main () {
     // suma_numeros_positivos()
     // buscar_elemento()
@@ -9,9 +11,36 @@ fun main () {
 
     // 11/05 - 04:47
     // encontrar_mayor()
-    calcular_primos()
+    // calcular_primos()
     // println(calcular_factorial(5))
     // println(calcular_factorial())
+
+    // 12/05
+    //// PARÁMETROS RETORNO ////
+    // var areaCirculo = calcular_area_circulo(3.0)
+
+    // val listaNumeros = listOf(2, 4, 26, 11)
+    // println(sumar_lista(listaNumeros))
+
+    // val listaNumeros2 = listOf(3 , 45 , 103 , 20)
+    // println(encontrar_maximo(listaNumeros2))
+
+    // val resultado = revertir_cadena("Estamos jodidos")
+    // println(resultado)
+
+    // val listaNumeros3 = listOf(23, 44, 56, 88)
+    // println(calcular_promedio(listaNumeros3))
+
+    // val resultado = es_palidromo("meow")
+    // println(resultado)
+
+    // calcular_potencia(15.0, 3)
+
+    // val frase = contar_vocales("Mi chica me da ideas")
+
+    // println(es_primo(111))
+
+    // calcularFactorial(5)
 
     println("Todos putos, me dan el curso a medias")
 }
@@ -126,20 +155,19 @@ fun encontrar_mayor () {    // Este sí, venga
 fun calcular_primos(): List<Int> {             // Me caguen sus muertos, ni con el ChatGPT lo saco
     var losPrimos = mutableListOf<Int>()
     var primoInicial = 2
-    for (n in 2..primoInicial){
+    for (n in 2 until primoInicial){
         if (primoInicial %n == 0){
             continue
         } else {
             losPrimos.add(primoInicial)
             continue
         }
+        primoInicial++
         if (losPrimos.size == 50) {
             break
         }
-        primoInicial++
     }
     return losPrimos
-    println(losPrimos)
 }
 
 fun calcular_factorial (numero: Int): Int {    // He mejorao pero sigo siendo tonto, lo hice con aiuda D;
@@ -154,4 +182,100 @@ fun calcular_factorial (numero: Int): Int {    // He mejorao pero sigo siendo to
         }
     }
     return factorial
+}
+
+fun calcular_area_circulo (radio: Double):Double{  // Me sirvió bien para aprender cosas, gg
+    var resultado = radio.pow(2) * Math.PI
+    println(resultado)
+    return resultado
+}
+
+fun sumar_lista (lista: List<Int>): Int {
+    var suma = 0
+    for (n in lista){
+        suma += n
+    }
+    return suma
+}
+
+fun encontrar_maximo (lista: List<Int>): Int {   // VAMOOOOOOS Sin ayuda!!
+    var maximo = 0
+    for (i in lista) {
+        if (i > maximo){
+            maximo = i
+            continue
+        } else {
+            continue
+        }
+    }
+    return maximo
+}
+
+fun revertir_cadena (cadena: String): String {   // No sé si sabría volver a hacerlo si me lo piden
+    var invertida = "" // Variable para guardar el texto que le demos
+    for (i in cadena.length -1 downTo 0){  // Empieza en la última posición y va bajando hasta la posición 0
+        invertida += cadena[i]
+    }
+    return invertida
+}
+
+fun calcular_promedio (lista: List<Int>): Double {   // Mi chica me dio el detalle que me faltaba :3
+    var valorInicial = 0.0
+    for (n in lista){
+        valorInicial += n
+    }
+    var media = valorInicial/2
+    return media
+}
+
+fun es_palidromo (texto: String): Boolean {   // Lo estoy entendiendo al final
+    var devolucion = false
+    var derecho = texto
+    var reves = ""
+    for (n in texto.length - 1 downTo 0){
+        reves += texto [n]
+        if (derecho == reves) {
+            devolucion = true
+        }
+    }
+    return devolucion
+}
+
+fun calcular_potencia (num1: Double , num2: Int): Double {  // Nota: los .pow son especialitos y no funcionan con enteros
+    var potencia = num1.pow(num2)
+    println(potencia)
+    return potencia
+}
+
+fun contar_vocales (texto: String): Int {  // Este es ejercicio de mirar apuntes
+    var acumulador = 0
+    for (n in texto){
+        if (n.lowercaseChar() in "aeiou"){
+            continue
+        }
+        acumulador ++
+    }
+    println(acumulador)
+    return acumulador
+}
+
+fun es_primo (numero: Int): Boolean {      // No declares más variables Booleanas, caguendios
+    if (numero < 2) {
+        return false
+    }
+    for (i in 2 until numero) {
+        if (numero %i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+fun calcularFactorial (numero: Int): Int {   // Este sin ayuda y sin mirar apuntes!!
+    var resultado = 1
+    for (i in 1..numero) {
+        resultado *= i
+    }
+    println(resultado)
+    return resultado
 }
